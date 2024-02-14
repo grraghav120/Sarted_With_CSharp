ff574493a65df8db5260d2f551949a976bedf972
Raghav Garg <grraghav120@gmail.com>
2024-02-14 20:13:52 +0530

    Time Changes also Done

--- DateExercise/DateTimeCS.cs
+++ DateExercise/DateTimeCS.cs
@@ -10,6 +10,11 @@ namespace ConsoleApp1.DateExercise
             Console.WriteLine(DateTime.Now.ToLongTimeString());
             Console.WriteLine("Tommorrow: {0}", DateTime.Now.AddDays(1));
             Console.WriteLine("Yesterday: {0}", DateTime.Now.AddDays(-1));
+            Console.WriteLine("Represent 1 Hour {0}",TimeSpan.FromHours(1));
+            Console.WriteLine("Represent 8 Minutes {0}",TimeSpan.FromMinutes(8));
+            var modifiedTime = TimeSpan.FromMinutes(8).Minutes;
+            var currentTime = DateTime.Now.Minute;
+            Console.WriteLine("currentTime - 8: {0}",(currentTime-modifiedTime));
         }
     }
 }
