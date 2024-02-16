88a62f5e9eef701dcf392e04c02923d86cc97523
Raghav Garg <grraghav120@gmail.com>
2024-02-16 12:25:21 +0530

    Refactor the Logic in Stopwatch Program

--- CsIntermediate/StopWatch.cs
+++ CsIntermediate/StopWatch.cs
@@ -5,11 +5,19 @@ namespace ConsoleApp1.CsIntermediate
     internal class StopWatch
     {
         private DateTime _interval;
+        private bool startStopWatch = false;
         public void Start() {
-            _interval = DateTime.Now;
+            if (startStopWatch)
+            {
+                throw new InvalidOperationException("Already Started Please First Stop the Watch:");
+            }
+            else {
+                _interval = DateTime.Now;
+                startStopWatch = true;
+            }
         }
         public void Stop() {
-
+            startStopWatch = false;
             var currentTime=DateTime.Now;
             var diff=currentTime - _interval;
             Console.WriteLine("\t Lap Recorded at {0}",diff);
