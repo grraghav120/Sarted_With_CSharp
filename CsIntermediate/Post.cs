f30bfceb9f4bbc32950671fff9103b12693870bb
Raghav Garg <grraghav120@gmail.com>
2024-02-16 12:52:56 +0530

    Created StackOverflow Post Program

---
+++ CsIntermediate/Post.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1.CsIntermediate
+{
+    internal class Post
+    {
+        private int _numberOfVotes=0;
+        private string _title;
+        private string _description;
+        private DateTime _postDate;
+
+        public Post(string title, string description, DateTime postDate)
+        {
+            this._title = title;
+            this._description = description;
+            this._postDate = postDate;
+        }
+
+        public void SetVotes(bool val)
+        {
+            if (val) this._numberOfVotes++;
+            else this._numberOfVotes--;
+        }
+
+        public int Vote
+        {
+            get {
+                return this._numberOfVotes;
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,20 +8,43 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            StopWatch sw=new StopWatch();
+            //StopWatch sw=new StopWatch();
             bool stopLoop = false;
+            //while (true)
+            //{
+            //    Console.WriteLine("Press \n 1 -> Start \n 2-> Stop \n 3-> Exit");
+            //    var input=Console.ReadLine();
+            //    switch (input)
+            //    {
+            //        case "1":
+            //            sw.Start();
+            //            Console.WriteLine("\t Started");
+            //            break;
+            //        case "2":
+            //            sw.Stop();
+            //            break;
+            //        default:
+            //            stopLoop = true;
+            //            break;
+            //    }
+            //    if (stopLoop) break;
+            //}
+
+            Post post = new Post("newPost","Description",DateTime.Now);
             while (true)
             {
-                Console.WriteLine("Press \n 1 -> Start \n 2-> Stop \n 3-> Exit");
-                var input=Console.ReadLine();
-                switch (input)
+                Console.WriteLine("Press \n 1-> Upvote \n 2-> Downvote \n 3-> Get the number of votes");
+                string input = Console.ReadLine();
+                switch(input)
                 {
                     case "1":
-                        sw.Start();
-                        Console.WriteLine("\t Started");
+                        post.SetVotes(true);
                         break;
                     case "2":
-                        sw.Stop();
+                        post.SetVotes(false);
+                        break;
+                    case "3":
+                        Console.WriteLine("Number of Current Vote on this post is {0}",post.Vote);
                         break;
                     default:
                         stopLoop = true;
